b26dabe015490877622d636b2f82b3da2f2f70d5
rimanov <rabenkralle@yandex.ru>
2020-01-29 16:10:01 +0300

    'lesson_2'

---
+++ lesson_2/lesson2_1.sql
@@ -0,0 +1,14 @@
+create database if not exists example;
+
+use example;
+
+drop table if exists users;
+create table users (
+	id INT unsigned,
+	name VARCHAR(255)
+);
+
+drop database if exists sample;
+create database sample;
+
+
